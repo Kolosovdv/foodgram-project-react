75fdcb112b9fae0f9e77c4f152ad85582adf7abe
Dmitry <kolosovdv@rambler.ru>
2022-05-23 16:58:18 +0300

    74

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -14,6 +14,7 @@ class UserAdmin(admin.ModelAdmin):
     search_fields = ('username', 'email',)
 
 
+
 class SubscriptionAdmin(admin.ModelAdmin):
     list_display = ('user', 'author')
     search_fields = ('user',)
