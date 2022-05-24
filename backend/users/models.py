9f45f4a3672737b2a947530ed29c1060ad85c409
Dmitry <kolosovdv@rambler.ru>
2022-05-24 15:09:59 +0300

    6

--- backend/users/models.py
+++ backend/users/models.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils.translation import gettext as _
 
+
 class User(AbstractUser):
     """Кастомная модель юзера."""
     USERNAME_FIELD = 'email'
