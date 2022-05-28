65b9557ae31992fc1d4d72a26b3171dec49d0d9f
Dmitry <kolosovdv@rambler.ru>
2022-05-29 00:14:16 +0300

    24

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -9,8 +9,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = '6#00va*^87%lw60zz@9frf0m&x)a&_g76)wjpm0e(8!svrng3h'
 
-#DEBUG = False
-DEBUG = True
+DEBUG = False
+#DEBUG = True
 ALLOWED_HOSTS = ['62.84.115.146', '127.0.0.1', 'localhost', 'backend']
 #ALLOWED_HOSTS = ['*']
 INSTALLED_APPS = [
