e76277f6bdac7f7ca87007625aef1908e403fa37
Dmitry <kolosovdv@rambler.ru>
2022-05-31 17:30:43 +0300

    2

--- backend/recipes/views.py
+++ backend/recipes/views.py
@@ -103,7 +103,7 @@ class RecipeViewSet(ModelViewSet):
                     'единица измерения': measurement_unit,
                     'количество': amount
                 }    
-        text = f'{str(shoplist)}'
+        text += f'{str(shoplist)}'
         response = HttpResponse(text, 'Content-Type: text/plane')
         response['Content-Disposition'] = 'attachment; filename="shoplist.txt"'
         return response
